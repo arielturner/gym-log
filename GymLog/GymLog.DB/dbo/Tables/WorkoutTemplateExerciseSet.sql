34529d406da7f7575215e7e9e2bc8440fd04a27e
arielturner <arieltrnr@gmail.com>
2025-04-15 15:38:34 -0400

    Add missing foreign key

--- GymLog/GymLog.DB/dbo/Tables/WorkoutTemplateExerciseSet.sql
+++ GymLog/GymLog.DB/dbo/Tables/WorkoutTemplateExerciseSet.sql
@@ -1,8 +1,10 @@
 ﻿CREATE TABLE [dbo].[WorkoutTemplateExerciseSet] (
     [WorkoutTemplateExerciseSetId] INT IDENTITY (1, 1) NOT NULL,
+    [WorkoutTemplateExerciseId]    INT NOT NULL,
     [Set]                          INT NOT NULL,
     [Reps]                         INT NOT NULL,
     [Intensity]                    INT NOT NULL,
-    PRIMARY KEY CLUSTERED ([WorkoutTemplateExerciseSetId] ASC)
+    PRIMARY KEY CLUSTERED ([WorkoutTemplateExerciseSetId] ASC),
+    CONSTRAINT [FK_WorkoutTemplateExerciseSet_WorkoutTemplateExercise] FOREIGN KEY ([WorkoutTemplateExerciseId]) REFERENCES [dbo].[WorkoutTemplateExercise] ([WorkoutTemplateExerciseId])
 );
 
