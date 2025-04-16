33a19cf2b80a0be5b7915b9c0bce2a7940b7873a
arielturner <arieltrnr@gmail.com>
2025-04-16 17:45:15 -0400

    Remove connection string in DbContext source code

--- GymLog/GymLog.DataAccess/GymLogContext.cs
+++ GymLog/GymLog.DataAccess/GymLogContext.cs
@@ -30,10 +30,6 @@ public partial class GymLogContext : DbContext
 
     public virtual DbSet<WorkoutTemplateExerciseSet> WorkoutTemplateExerciseSets { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP\\SQLEXPRESS;Initial Catalog=GymLog;TrustServerCertificate=True;Trusted_Connection=True");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<BodyPart>(entity =>
