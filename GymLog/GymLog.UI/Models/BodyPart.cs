1aecbefb8a5e77f4c453480ba9c707310593fb0e
arielturner <arieltrnr@gmail.com>
2025-07-06 18:12:22 -0400

    add data annotations for BodyPart model

--- GymLog/GymLog.UI/Models/BodyPart.cs
+++ GymLog/GymLog.UI/Models/BodyPart.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GymLog.UI.Models;
 
 public partial class BodyPart
 {
+    [Display(Name = "ID")]
     public int BodyPartId { get; set; }
 
-    public string BodyPartName { get; set; } = null!;
+    [Required]
+    [StringLength(50)]
+    [Display(Name = "Body Part Name")]
+    public string BodyPartName { get; set; }
 
     public DateTime CreatedAt { get; set; }
 
+    [Required]
+    [StringLength(50)]
     public string CreatedBy { get; set; } = null!;
 
     public DateTime UpdatedAt { get; set; }
 
+    [Required]
+    [StringLength(50)]
     public string UpdatedBy { get; set; } = null!;
 
     public virtual ICollection<Exercise> Exercises { get; set; } = new List<Exercise>();
